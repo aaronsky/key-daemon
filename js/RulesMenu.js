f0fec91ceb3c8d828cf1100987c22d2693cadd2a
Aaron Sky <aaronsky@skyaaron.com>
2014-10-21 09:13:50 -0400

    Created basic EndScreen, renamed RuleFadeIn to fit class name, changed how Core.currentLevel and RulesMenu play

--- js/RulesMenu.js
+++ js/RulesMenu.js
@@ -3,6 +3,8 @@
 
 function RulesMenu(centerX, centerY, time) {
     this.rect = {
+        width: centerX * 2,
+        height: centerY * 2,
         centerX: centerX,
         centerY: centerY
     };
@@ -13,8 +15,8 @@ function RulesMenu(centerX, centerY, time) {
                                    time,
                                    "#DCDCDB",
                                    "#6D6D6D",
-                                   this.rect.centerX * 2 - 230,
-                                   this.rect.centerY * 2 - 230);
+                                   this.rect.width - 230,
+                                   this.rect.height - 230);
     this.spinner.isOn(true);
     this.rulesTexts = [
         new RuleFadeIn(50, 50, 3, "Pick A Color"),
@@ -22,7 +24,7 @@ function RulesMenu(centerX, centerY, time) {
         new RuleFadeIn(50, 250, 3, "Use One Finger"),
         new RuleFadeIn(50, 350, 3, "You Have 2 Minutes")
     ];
-    
+
 }
 
 RulesMenu.prototype = {constructor: RulesMenu};
@@ -36,38 +38,39 @@ RulesMenu.prototype.animate = function () {
 };
 
 RulesMenu.prototype.update = function () {
-    var i = 0,
-        len = this.rulesTexts.length;
-    for (i; i < len; i += 1) {
-        this.rulesTexts[i].update(1);
-        if (!this.rulesTexts[i].done) {
-            break;
-        }
-    }
-    this.spinner.update();
-
-};
-
-RulesMenu.prototype.draw = function (ctx) {
-    if (!this.spinner.isDone()) {
-        ctx.fillStyle = this.backgroundColor;
-        ctx.fillRect(0,
-                     0,
-                     this.rect.centerX * 2,
-                     this.rect.centerY * 2);
-
-        ctx.textAlign = 'left';
-        ctx.textBaseline = 'hanging';
-        var textSize = 30,
-            colorWidth = ctx.measureText("Pick A Color").width + 50;
-        ctx.font = 'normal ' + textSize + 'pt Raleway Light';
-
+    if (this.spinner.isDone()) {
+        Core.getInstance().currentLevel = new Level(4, this.rect.centerX, this.rect.centerY);
+    } else {
         var i = 0,
             len = this.rulesTexts.length;
         for (i; i < len; i += 1) {
-            this.rulesTexts[i].draw(ctx);
+            this.rulesTexts[i].update(1);
+            if (!this.rulesTexts[i].done) {
+                break;
+            }
         }
-
-        this.spinner.draw(ctx);
+        this.spinner.update();
     }
+};
+
+RulesMenu.prototype.draw = function (ctx) {
+    ctx.fillStyle = this.backgroundColor;
+    ctx.fillRect(0,
+                 0,
+                 this.rect.width,
+                 this.rect.height);
+
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'hanging';
+    var textSize = 30,
+        colorWidth = ctx.measureText("Pick A Color").width + 50;
+    ctx.font = 'normal ' + textSize + 'pt Raleway Light';
+
+    var i = 0,
+        len = this.rulesTexts.length;
+    for (i; i < len; i += 1) {
+        this.rulesTexts[i].draw(ctx);
+    }
+
+    this.spinner.draw(ctx);
 };
